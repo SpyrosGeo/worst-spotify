a398d85b5b4242c1524e8b2789e1ce4e7a12b297
SpyrosGeo <xarodasimba@gmail.com>
2021-03-17 17:20:41 +0200

    cant find where the authentication error lies//TODO fix that

--- server/server.js
+++ server/server.js
@@ -1,30 +1,38 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const SpotifyWebApi = require('spotify-web-api-node')
 require('dotenv').config()
 const app = express()
-
+app.use(cors())
+app.use(express.json())
+// app.use(bodyParser.urlencoded({
+//     extended:true
+// }))
 const PORT = 4000;
 
 
-app.post('/', async (req, res) => {
-    const code = req.body.code
-    const spotifyWebApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: `${process.env.CLIENT_ID}`,
-        clientSecret: `${process.env.CLIENT_SECRET}`
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
     })
-
-    try {
-        const data = await spotifyWebApi.authorizationCodeGrant(code)
-        res.json({
-            accessToken: data.access_token,
-            refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in
-        })
-
-    } catch (error) {
-        res.sendStatus(400)
-    }
+    .catch(err => {
+        console.log(err)
+      res.sendStatus(400)
+    })
 })
 
 
